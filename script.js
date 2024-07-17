d6933301c1c89f09c640527541cb3a9213dad0d8
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-07-17 15:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-//complete this code
-class Animal {}
+//complete this code// Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make sound
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Cat class extending Animal
+class Cat extends Animal {
+  // Method to purr
+  purr() {
+    console.log('purr');
+  }
+}
+
+// Dog class extending Animal
+class Dog extends Animal {
+  // Method to bark
+  bark() {
+    console.log('woof');
+  }
+}
+
+// Example usage
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
